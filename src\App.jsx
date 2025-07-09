69ef875bc977db12f1cabddc43d1108174d6bdec
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-09 21:42:31 +0500

    Add src\App.jsx

---
+++ src\App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Hero from './Hero';
+import Features from './Features';
+import Footer from './Footer';
+
+const App = () => {
+  return (
+    <div className="min-h-screen">
+      <Hero />
+      <Features />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
